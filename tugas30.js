730ba494b525e42097945ab344314bf211b24f76
Havid <great_havid@yahoo.co.id>
2019-11-13 11:49:59 +0700

    Tugas 30 JS

---
+++ tugas30.js
@@ -0,0 +1,8 @@
+// ====== Tugas 30 JavaScript =====
+
+function pRegex(value){
+    var data = value.match(/[es]/g);
+    console.log(data);
+}
+
+pRegex( "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
